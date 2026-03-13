2191e1483a1a65095929bb55b40233442fb2ccda
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-03-13 15:58:19 +0600

    storage

---
+++ 30.5.3&4-A_Simple_Shopping_Cart_to_Add_Product_and_Quantity/cart.js
@@ -0,0 +1,76 @@
+
+const handleAddProducts = () => {
+    const productEl = document.getElementById("product");
+    const quantityEl = document.getElementById("quantity");
+    const product = productEl.value;
+    const quantity = parseInt(quantityEl.value);
+    // console.log("Products Add", product, quantity);
+
+    displayProduct(product, quantity);
+    addProductToCart(product, quantity)
+
+    productEl.value = "";
+    quantityEl.value = "";
+}
+
+const getCart = () => {
+    let cart = {};
+
+    const cartJSON = localStorage.getItem("cart");
+    console.log(cartJSON);
+    if(cartJSON){
+        cart = JSON.parse(cartJSON);
+    }
+    
+    return cart;
+}
+
+const addProductToCart = (product, quantity) => {
+    const cart = getCart();
+
+    if(cart[product]){
+        cart[product] = cart[product] + quantity;
+    }
+    else{
+        cart[product] = quantity;
+    }
+
+
+
+    console.log("cart", cart);
+    const cartJSON = JSON.stringify(cart);
+    localStorage.setItem("cart", cartJSON)
+}
+
+const displayProduct = (product, quantity) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    ${product} : ${quantity}
+    `
+    // get the ul
+    const ul = document.getElementById("products-container");
+    ul.appendChild(li);
+}
+
+// display products from stored Local Storage
+const displayStoredProducts = () => {
+    const cart = getCart();
+    for(const product in cart) {
+        const quantity = cart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity)
+    }
+}
+displayStoredProducts();
+
+/**
+ * To save object/array in the local storage
+ * 1. convert the object to JSON string by using JSON.stringify
+ * 2. localstorage.setItem()
+ */
+
+/**
+ * to get object/array from the local storage 
+ * 1. get the item from the local storage and it will be JSON string
+ * 2. convert the JSON to js object by using JSON.perse
+ */
--- js/local-storage.js
+++
@@ -1,5 +0,0 @@
-const addNumberToLS = () => {
-    const number = Math.ceil(Math.random()*100);
-    console.log(number);
-    localStorage.setItem("number", number)
-}
